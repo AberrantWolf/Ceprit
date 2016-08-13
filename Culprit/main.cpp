8530abcbf330ba7ad1dc32418a2bc9cd81015844
Scott Harper <orcein@gmil.com>
2016-08-14 07:15:44 +0900

    Initial Commit

---
+++ Culprit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Culprit
+//
+//  Created by B. Scott Harper on 8/14/16.
+//  Copyright © 2016 Harpers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
